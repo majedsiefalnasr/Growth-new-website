c624f0da29a7152b3915e41f9ec36e1ad72246d1
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2023-01-11 11:26:22 +0200

    On stage

---
+++ README.md
@@ -0,0 +1,21 @@
+<br />
+<p align="center">
+  <img src="./dist/docs/src/readme.png">
+</p>
+<br />
+
+<h1 align="center">Growth - Dashboard</h1>
+
+<h3 align="center">Without technical experience and with ease, you can sell anywhere and anytime with your online store and with your commercial identity</h3>
+
+<br /> <br />
+
+# Ultimate Solutions
+
+### International leader in Enterprise Resource Solutions and Software Development
+
+If you're the kind of person who reads source code, you should visit [Our Site](https://ultimate-eg.net) for more awesome things.
+
+## License
+
+© 2020 All Rights Reserved by [Ultimate Solutions](https://ultimatesolutionsportal.com/).
---
+++ dist/js/app.js
@@ -0,0 +1,27 @@
+('use strict');
+
+// Global variables
+window.APPData = {
+  INFO: {
+    PROVIDER: 'Ultimate Solutions Egypt',
+    PROVIDER_INFO: 'International leader in Enterprise Resource Solutions and Software Development',
+    PROVIDER_LINK: 'https://ultimate-eg.net',
+    PROJECT: 'HOLOL - Smart shopping platform',
+    PROJECT_INFO:
+      'Without technical experience and with ease, you can sell anywhere and anytime with your online store and with your commercial identity',
+    VERSION: 'beta',
+  },
+  LOCAL: {
+    BROWSER: {
+      LANGUAGE: '',
+    },
+  },
+  UTIL: {
+    DelegatedEventHandlers: {},
+  },
+  ERROR: {
+    HELP_CENTER: '/help-center',
+    LOAD_SCRIPT: '',
+  },
+  LAZYLOAD_INSTANCE: '',
+};
