8a4b8d410f043e874eb20a68670772006e0bb54e
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2023-02-07 13:05:21 +0200

    Add Hero section action

--- dist/js/app.js
+++ dist/js/app.js
@@ -31,6 +31,9 @@ SUtility.onDOMContentLoaded(() => {
 
   // Navbar
   navbar();
+
+  // Hero futures
+  hero_futures();
 });
 
 //    ____                      _        _          _ _
@@ -345,5 +348,33 @@ function navbar() {
       navbar.style.cssText =
         ` padding-` + browserDir + `: ` + (currentWidth - prevWidth + 24) + `px; `;
     }
+  }
+}
+
+//   _    _                   __       _
+//  | |  | |                 / _|     | |
+//  | |__| | ___ _ __ ___   | |_ _   _| |_ _   _ _ __ ___  ___
+//  |  __  |/ _ \ '__/ _ \  |  _| | | | __| | | | '__/ _ \/ __|
+//  | |  | |  __/ | | (_) | | | | |_| | |_| |_| | | |  __/\__ \
+//  |_|  |_|\___|_|  \___/  |_|  \__,_|\__|\__,_|_|  \___||___/
+//
+//
+// Hero futures
+function hero_futures() {
+  if (document.querySelector('#hero-block .futures')) {
+    SUtility.each(document.querySelectorAll('#hero-block .futures .item'), function (target) {
+      SUtility.addEvent(target, 'click', () => {
+        if (!SUtility.hasClass(target, 'active')) {
+          // Remove active from prev item
+          SUtility.removeClass(
+            document.querySelector('#hero-block .futures .item.active'),
+            'active'
+          );
+
+          // Add active to target
+          SUtility.addClass(target, 'active');
+        }
+      });
+    });
   }
 }
