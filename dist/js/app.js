069647f9152dd00478342faa92a6c13a407aec8d
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2023-02-12 16:59:42 +0200

    Update Pricing page & add Pricing features page

--- dist/js/app.js
+++ dist/js/app.js
@@ -37,6 +37,12 @@ SUtility.onDOMContentLoaded(() => {
 
   // Floating Actions
   floating_actions();
+
+  // Pricing switcher
+  pricing_switcher_actions();
+
+  // Pricing plan mobile switcher
+  pricing_plan_switcher_actions();
 });
 
 //    ____                      _        _          _ _
@@ -442,5 +448,101 @@ function floating_actions() {
   // On click back to top action
   SUtility.addEvent(backToTop, 'click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+}
+
+//   _____      _      _                            _ _       _
+//  |  __ \    (_)    (_)                          (_) |     | |
+//  | |__) | __ _  ___ _ _ __   __ _   _____      ___| |_ ___| |__   ___ _ __
+//  |  ___/ '__| |/ __| | '_ \ / _` | / __\ \ /\ / / | __/ __| '_ \ / _ \ '__|
+//  | |   | |  | | (__| | | | | (_| | \__ \\ V  V /| | || (__| | | |  __/ |
+//  |_|   |_|  |_|\___|_|_| |_|\__, | |___/ \_/\_/ |_|\__\___|_| |_|\___|_|
+//                              __/ |
+//                             |___/
+// Pricing switcher
+function pricing_switcher_actions() {
+  var container = document.querySelector('.switch-wrapper'),
+    plans = document.querySelector('.switch-wrapper ~ .plans '),
+    plans_table = document.querySelector('.switch-wrapper ~ .plans--table'),
+    monthly = container.querySelector('#monthly'),
+    yearly = container.querySelector('#yearly');
+
+  // Check if monthly is checked
+  SUtility.addEvent(monthly, 'change', () => {
+    if (monthly.checked) {
+      // Remove yearly check
+      SUtility.removeClass(plans, 'yearly');
+      SUtility.removeClass(plans_table, 'yearly');
+
+      // Add monthly check
+      SUtility.addClass(plans, 'monthly');
+      SUtility.addClass(plans_table, 'monthly');
+    }
+  });
+
+  // Check if yearly is checked
+  SUtility.addEvent(yearly, 'change', () => {
+    if (yearly.checked) {
+      // Remove monthly check
+      SUtility.removeClass(plans, 'monthly');
+      SUtility.removeClass(plans_table, 'monthly');
+
+      // Add yearly check
+      SUtility.addClass(plans, 'yearly');
+      SUtility.addClass(plans_table, 'yearly');
+    }
+  });
+}
+
+//   _____      _      _                     _
+//  |  __ \    (_)    (_)                   | |
+//  | |__) | __ _  ___ _ _ __   __ _   _ __ | | __ _ _ __
+//  |  ___/ '__| |/ __| | '_ \ / _` | | '_ \| |/ _` | '_ \
+//  | |   | |  | | (__| | | | | (_| | | |_) | | (_| | | | |
+//  |_|   |_|  |_|\___|_|_|_|_|\__, | | .__/|_|\__,_|_| |_|  _
+//                  | |   (_) | __/ | | |         (_) |     | |
+//   _ __ ___   ___ | |__  _| ||___/  |_|__      ___| |_ ___| |__   ___ _ __
+//  | '_ ` _ \ / _ \| '_ \| | |/ _ \ / __\ \ /\ / / | __/ __| '_ \ / _ \ '__|
+//  | | | | | | (_) | |_) | | |  __/ \__ \\ V  V /| | || (__| | | |  __/ |
+//  |_| |_| |_|\___/|_.__/|_|_|\___| |___/ \_/\_/ |_|\__\___|_| |_|\___|_|
+//
+//
+// Pricing plan mobile switcher
+function pricing_plan_switcher_actions() {
+  var container = document.querySelector('.switch-wrapper ~ .plans--table .table--nav'),
+    links = container.querySelectorAll('a[href]');
+
+  // Loop all switcher
+  SUtility.each(links, (item) => {
+    // Scroll on click
+    SUtility.addEvent(item, 'click', () => {
+      event.preventDefault();
+
+      // Get target ID
+      let element = document.querySelector(SUtility.attr(item, 'href'));
+      let headerOffset = 220;
+      let elementPosition = element.getBoundingClientRect().top;
+      let offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
+    });
+
+    // Active on scroll to target
+    document.addEventListener('scroll', (event) => {
+      // Target element
+      let target = document.querySelector(SUtility.attr(item, 'href'));
+
+      if (SUtility.isPartInViewport(target)) {
+        // Remove active status from old tab
+        if (container.querySelector('a[href].active'))
+          SUtility.removeClass(container.querySelector('a[href].active'), 'active');
+
+        // Add active status to current tab
+        SUtility.addClass(item, 'active');
+      }
+    });
   });
 }
