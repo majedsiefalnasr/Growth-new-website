025cd0dc81f5594fbd6ef72b176e2a2fc10a72fc
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2023-03-06 13:20:05 +0200

    Add Parallax actions

--- dist/js/app.js
+++ dist/js/app.js
@@ -70,6 +70,9 @@ SUtility.onDOMContentLoaded(() => {
 
   // Help Center actions
   help_center_actions();
+
+  // Parallax
+  parallax();
 });
 
 //    ____                      _        _          _ _
@@ -1236,5 +1239,56 @@ function magnetic() {
         duration: speed,
       });
     }
+  }
+}
+
+//   _____                _ _
+//  |  __ \              | | |
+//  | |__) |_ _ _ __ __ _| | | __ ___  __
+//  |  ___/ _` | '__/ _` | | |/ _` \ \/ /
+//  | |  | (_| | | | (_| | | | (_| |>  <
+//  |_|   \__,_|_|  \__,_|_|_|\__,_/_/\_\
+//
+//
+// Parallax
+function parallax() {
+  // Get parallax container
+  var parallax_container = document.querySelector('[data-parallax]');
+
+  // Check if target is exist
+  if (parallax_container) {
+    (function () {
+      // Get Moving parent
+      let parallax_parent = document.querySelector(
+        SUtility.attr(parallax_container, 'data-parallax')
+      );
+
+      // The elements you want to parallax.
+      var parallax_items = parallax_container.querySelectorAll('.parallax');
+
+      // Use this value to adjust the amount of parallax in response to mouse movement.
+      var fixer = 0.003;
+
+      parallax_parent.addEventListener('mousemove', function (event) {
+        // get the mouseX - negative on left, positive on right
+        var pageX = event.pageX - parallax_container.getBoundingClientRect().width * 0.5;
+
+        // same here, get the y. use console.log(pageY) to see the values
+        var pageY = event.pageY - parallax_container.getBoundingClientRect().height * 0.5;
+
+        for (let i = 0; i < parallax_items.length; i++) {
+          var item = parallax_items[i];
+          var speedX = item.getAttribute('data-speed-x');
+          var speedY = item.getAttribute('data-speed-y');
+
+          // Instead of using 'TweenLite.set' you can use 'TweenLite.to' which results in a smoother initial transition (when the mouse enters) at the cost of some preformance.
+          // Change the '+' to '-' if you want to invert the parallax motion in relation to the mouse movement.
+          TweenLite.set(item, {
+            x: (item.offsetLeft + pageX * speedX) * fixer,
+            y: (item.offsetTop + pageY * speedY) * fixer,
+          });
+        }
+      });
+    })();
   }
 }
