8eb54bd5db18de3ec3270970032088166a992833
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2023-03-14 16:47:15 +0200

    Add HC-Sticky and update all scripts calling

--- dist/js/app.js
+++ dist/js/app.js
@@ -74,6 +74,9 @@ SUtility.onDOMContentLoaded(() => {
   // Parallax
   parallax();
 
+  // HC-Sticky
+  HC_Sticky();
+
   // Forms
   forms();
 });
@@ -1343,6 +1346,89 @@ function parallax() {
         }
       });
     })();
+  }
+}
+
+//   _    _  _____      _____ _   _      _
+//  | |  | |/ ____|    / ____| | (_)    | |
+//  | |__| | |   _____| (___ | |_ _  ___| | ___   _
+//  |  __  | |  |______\___ \| __| |/ __| |/ / | | |
+//  | |  | | |____     ____) | |_| | (__|   <| |_| |
+//  |_|  |_|\_____|   |_____/ \__|_|\___|_|\_\\__, |
+//                                             __/ |
+//                                            |___/
+// HC-Sticky
+function HC_Sticky() {
+  // Get HC-Sticky container
+  var HC_Sticky_containers = document.querySelectorAll('[data-HC-Sticky]');
+
+  // Check if target is exist
+  if (HC_Sticky_containers) {
+    // Loop all HC Sticky
+    SUtility.each(HC_Sticky_containers, (container) => {
+      let HC_Sticky_top = SUtility.attr(container, 'data-HC-Sticky-top') || 0,
+        HC_Sticky_bottom = SUtility.attr(container, 'data-HC-Sticky-bottom') || 0,
+        HC_Sticky_innerTop = SUtility.attr(container, 'data-HC-Sticky-innerTop') || 0,
+        HC_Sticky_bottomEnd = SUtility.attr(container, 'data-HC-Sticky-bottomEnd') || 0,
+        HC_Sticky_disable = SUtility.hasAttr(container, 'data-HC-Sticky-disable') || false,
+        HC_Sticky_disable_sm = SUtility.hasAttr(container, 'data-HC-Sticky-disable-sm') || false,
+        HC_Sticky_disable_md = SUtility.hasAttr(container, 'data-HC-Sticky-disable-md') || false,
+        HC_Sticky_disable_lg = SUtility.hasAttr(container, 'data-HC-Sticky-disable-lg') || false,
+        HC_Sticky_disable_xl = SUtility.hasAttr(container, 'data-HC-Sticky-disable-xl') || false,
+        HC_Sticky_onStart = SUtility.attr(container, 'data-HC-Sticky-onStart') || null,
+        HC_Sticky_onStop = SUtility.attr(container, 'data-HC-Sticky-onStop') || null,
+        HC_Sticky_onBeforeResize =
+          SUtility.attr(container, 'data-HC-Sticky-onBeforeResize') || null,
+        HC_Sticky_onResize = SUtility.attr(container, 'data-HC-Sticky-onResize') || null;
+
+      var Sticky = new hcSticky(container, {
+        // The distance from the top
+        top: HC_Sticky_top,
+        // The distance from the bottom
+        bottom: HC_Sticky_bottom,
+        // The distance from the bottom of the referring element
+        bottomEnd: HC_Sticky_bottomEnd,
+        // The distance from the top inside of the sticky content
+        innerTop: HC_Sticky_innerTop,
+        // Element at which to trigger HC-Sticky instead of top of sticky container.
+        // This has higher priority than innerTop option.
+        // innerSticker: null,
+        // Classname of sticky element
+        // stickyClass: 'sticky',
+        // Classname of wrapper
+        // wrapperClassName: 'wrapper-sticky',
+        // Element that represents the reference for height instead of height of the container. recalculate
+        // stickTo: null,
+        // Object containing responsive breakpoints
+        responsive: {
+          576: {
+            disable: HC_Sticky_disable_sm,
+          },
+          768: {
+            disable: HC_Sticky_disable_md,
+          },
+          992: {
+            disable: HC_Sticky_disable_lg,
+          },
+          1200: {
+            disable: HC_Sticky_disable_xl,
+          },
+        },
+        // When set to false, sticky content will not move with the page if it is bigger that Window.
+        // followScroll: true,
+        // Direction of the responsive queries.
+        // mobileFirst: true,
+        // Limit the rate at which the HC Sticky can fire on window resize.
+        // resizeDebounce: 100,
+        // Disable the plugin
+        disable: HC_Sticky_disable,
+        // Callbacks
+        onStart: HC_Sticky_onStart,
+        onStop: HC_Sticky_onStop,
+        onBeforeResize: HC_Sticky_onBeforeResize,
+        onResize: HC_Sticky_onResize,
+      });
+    });
   }
 }
 
