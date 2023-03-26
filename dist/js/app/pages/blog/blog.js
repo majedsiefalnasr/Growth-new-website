814657138f6e0595d7d5edc2f70b7af89a6b218e
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2023-03-26 11:32:36 +0200

    Update Blog and related pages OOP

--- dist/js/app/pages/blog/blog.js
+++ dist/js/app/pages/blog/blog.js
@@ -1,5 +1,8 @@
 ('use strict');
 
+// Utilities
+import { SUtility } from '../../utilities/utility.js';
+
 //   ____  _
 //  |  _ \| |
 //  | |_) | | ___   __ _
@@ -46,6 +49,39 @@ export var blog = function () {
     import('../../utilities/splide.js')
       .then(({ splide }) =>
         splide(splide_hero_container, {
+          type: 'slide',
+          autoplay: false,
+          perPage: 1,
+          breakpoints: {
+            767.98: {
+              gap: 32,
+              perPage: 1.2,
+            },
+          },
+          perMove: 1,
+          direction: SUtility.getDir(),
+          gap: 64,
+          speed: 800,
+          arrows: true,
+          pagination: true,
+          lazyLoad: 'nearby',
+          focus: 'center',
+          omitEnd: true,
+          trimSpace: false,
+          arrowPath:
+            'M24.7264 30.667C24.6229 30.7725 24.4994 30.8562 24.3631 30.9134C24.2269 30.9706 24.0806 31 23.9329 31C23.7851 31 23.6388 30.9706 23.5026 30.9134C23.3664 30.8562 23.2429 30.7725 23.1394 30.667C22.9332 30.4575 22.8177 30.1754 22.8177 29.8815C22.8177 29.5876 22.9332 29.3055 23.1394 29.096L31.1864 21.049L4.63237 21.049C4.01337 21.049 3.50537 20.557 3.50537 19.938C3.50537 19.319 4.01337 18.811 4.63237 18.811L31.1864 18.811L23.1394 10.779C22.7104 10.335 22.7104 9.62004 23.1394 9.19204C23.2429 9.08661 23.3664 9.00286 23.5026 8.94569C23.6388 8.88852 23.7851 8.85908 23.9329 8.85908C24.0806 8.85908 24.2269 8.88852 24.3631 8.94569C24.4994 9.00286 24.6229 9.08661 24.7264 9.19204L34.6784 19.144C34.8845 19.3535 35 19.6356 35 19.9295C35 20.2234 34.8845 20.5055 34.6784 20.715L24.7264 30.667Z',
+        })
+      )
+      .catch((err) => console.log('error:', err));
+
+  // Add splide testimonials
+  let splide_blog_container = document.querySelector('[splide][splide-blog-hero]');
+
+  // Load script only on needed
+  splide_blog_container &&
+    import('../../utilities/splide.js')
+      .then(({ splide }) =>
+        splide(splide_blog_container, {
           type: 'slide',
           autoplay: false,
           perPage: 1,
